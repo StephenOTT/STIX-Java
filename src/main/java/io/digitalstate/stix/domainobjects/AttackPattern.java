80493a060d9943fe18b03d2f7e419ec250ec5f52
StephenOTT <stephenrussett@gmail.com>
2018-11-26 16:41:51 -0500

    refactor SDO common properties deserializer validators into seperate class

--- src/main/java/io/digitalstate/stix/domainobjects/AttackPattern.java
+++ src/main/java/io/digitalstate/stix/domainobjects/AttackPattern.java
@@ -8,16 +8,12 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import io.digitalstate.stix.domainobjects.properties.AttackPatternProperties;
 import io.digitalstate.stix.domainobjects.types.KillChainPhase;
-import io.digitalstate.stix.helpers.StixDataFormats;
-import org.camunda.commons.utils.IoUtilException;
 
 import java.io.IOException;
-import java.time.Instant;
-import java.time.ZoneId;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Optional;
 
+import static io.digitalstate.stix.domainobjects.json.CommonPropertiesDeserializationValidators.*;
 import static io.digitalstate.stix.helpers.IdGeneration.generateUuidAsString;
 
 @JsonDeserialize(using = AttackPattern.Deserializer.class)
@@ -55,75 +51,7 @@ public class AttackPattern extends AttackPatternProperties implements StixDomain
 
             AttackPattern object = new AttackPattern();
 
-            Optional<JsonNode> type = Optional.ofNullable(node.get("type"));
-            type.ifPresent(o -> {
-                if (!o.isNull() && o.asText().equals(TYPE)) {
-                    object.setType(o.asText());
-                } else {
-                    throw new IllegalArgumentException("Invalid Type: " + o.asText());
-                }
-            });
-            type.orElseThrow(() -> new IllegalArgumentException("Type is Required"));
-
-            Optional<JsonNode> id = Optional.ofNullable(node.get("id"));
-            id.ifPresent(o -> {
-                if (!o.isNull()) {
-                    object.setId(o.asText());
-                } else {
-                    throw new IllegalArgumentException("Invalid Id, if is null");
-                }
-            });
-            id.orElseThrow(() -> new IllegalArgumentException("Id is Required"));
-
-            Optional<JsonNode> created_by_ref = Optional.ofNullable(node.get("created_by_ref"));
-            created_by_ref.ifPresent(o -> {
-
-            });
-
-            Optional<JsonNode> created = Optional.ofNullable(node.get("created"));
-            created.ifPresent(o -> {
-                Instant instant = Instant.parse(o.asText());
-                object.setCreated(instant.atZone(ZoneId.of(StixDataFormats.DATETIMEZONE)));
-            });
-            created.orElseThrow(() -> new IllegalArgumentException("Created is Required"));
-
-            Optional<JsonNode> modified = Optional.ofNullable(node.get("modified"));
-            modified.ifPresent(o -> {
-                Instant instant = Instant.parse(o.asText());
-                object.setModified(instant.atZone(ZoneId.of(StixDataFormats.DATETIMEZONE)));
-            });
-            modified.orElseThrow(() -> new IllegalArgumentException("Modified is Required"));
-
-            Optional<JsonNode> revoked = Optional.ofNullable(node.get("revoked"));
-            revoked.ifPresent(o -> {
-                object.setRevoked(o.booleanValue());
-            });
-
-            Optional<JsonNode> labels = Optional.ofNullable(node.get("labels"));
-            labels.ifPresent(o -> {
-                if (o.isArray()) {
-                    LinkedHashSet<String> list = new LinkedHashSet<>();
-                    o.forEach(item -> {
-                        list.add(item.asText());
-                    });
-                    object.setLabels(list);
-                }
-            });
-
-            Optional<JsonNode> external_references = Optional.ofNullable(node.get("external_references"));
-            external_references.ifPresent(o -> {
-
-            });
-
-            Optional<JsonNode> object_marking_refs = Optional.ofNullable(node.get("object_marking_refs"));
-            object_marking_refs.ifPresent(o -> {
-
-            });
-
-            Optional<JsonNode> granular_markings = Optional.ofNullable(node.get("granular_markings"));
-            granular_markings.ifPresent(o -> {
-
-            });
+            validateAllCommonProperties(node, object, TYPE);
 
             Optional<JsonNode> targets = Optional.ofNullable(node.get("targets"));
             targets.ifPresent(o -> {
@@ -134,6 +62,13 @@ public class AttackPattern extends AttackPatternProperties implements StixDomain
             uses.ifPresent(o -> {
 
             });
+
+            Optional<JsonNode> name = Optional.ofNullable(node.get("name"));
+            name.ifPresent(o -> {
+                object.setName(o.textValue());
+            });
+            name.orElseThrow(()-> new IllegalArgumentException("Name is required"));
+
 
             Optional<JsonNode> description = Optional.ofNullable(node.get("description"));
             description.ifPresent(o -> {
