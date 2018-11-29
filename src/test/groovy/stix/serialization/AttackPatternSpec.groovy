4e19b819a4b3b7d4fce6aad611711c96e9cd9d2d
StephenOTT <stephenrussett@gmail.com>
2018-11-29 10:23:37 -0500

    Cleanup Unit test example to use single file and remove from file

--- src/test/groovy/stix/serialization/AttackPatternSpec.groovy
+++ src/test/groovy/stix/serialization/AttackPatternSpec.groovy
@@ -12,88 +12,10 @@ import java.time.ZoneId
 
 class AttackPatternSpec extends Specification {
 
-    def "AttackPattern 1 json"(){
+    def "AttackPattern Full 1"(){
 
-        when: "load AttackPattern1.json"
-        URL jsonUrl = getClass().getResource('/stix/json/domainobjects/AttackPattern1.json')
-
-        ObjectMapper objectMapper = StixDataFormats.getJsonMapper()
-
-        AttackPattern attackPattern = objectMapper.readValue(jsonUrl, AttackPattern.class)
-
-        then:
-        assert attackPattern.getType() == 'attack-pattern'
-        assert attackPattern.getId() == 'attack-pattern--477b763e-226f-46b3-a211-3cb5b86978a6'
-        assert attackPattern.getCreated() == Instant.parse('2018-11-26T01:17:26.416Z').atZone(ZoneId.of("Etc/UTC"))
-        assert attackPattern.getModified() == Instant.parse('2018-11-29T01:17:26.416Z').atZone(ZoneId.of("Etc/UTC"))
-        assert !attackPattern.revoked
-        assert attackPattern.getName() == "some pattern"
-
-    }
-
-    def "AttackPattern 2 json"(){
-
-        when: "load AttackPattern2.json"
-        URL jsonUrl = getClass().getResource('/stix/json/domainobjects/AttackPattern2.json')
-
-        ObjectMapper objectMapper = StixDataFormats.getJsonMapper()
-
-        AttackPattern attackPattern = objectMapper.readValue(jsonUrl, AttackPattern.class)
-
-        then:
-        assert attackPattern.getType() == 'attack-pattern'
-        assert attackPattern.getId() == 'attack-pattern--477b763e-226f-46b3-a211-3cb5b86978a6'
-        assert attackPattern.getCreated() == Instant.parse('2018-11-26T01:17:26.416Z').atZone(ZoneId.of("Etc/UTC"))
-        assert attackPattern.getModified() == Instant.parse('2018-11-29T01:17:26.416Z').atZone(ZoneId.of("Etc/UTC"))
-        assert attackPattern.revoked == false
-        assert attackPattern.getName() == "some pattern"
-
-
-        assert attackPattern.getKillChainPhases().size() == 2
-
-        LinkedHashSet<KillChainPhase> kcpSet = new LinkedHashSet<>();
-        kcpSet.add(new KillChainPhase("Chain1", "phase1"))
-        kcpSet.add(new KillChainPhase("Chain1", "phase2"))
-        assert attackPattern.getKillChainPhases() == kcpSet
-
-    }
-
-    def "AttackPattern 3 json"(){
-
-        when: "load AttackPattern3.json"
-        URL jsonUrl = getClass().getResource('/stix/json/domainobjects/AttackPattern3.json')
-
-        ObjectMapper objectMapper = StixDataFormats.getJsonMapper()
-
-        AttackPattern attackPattern = objectMapper.readValue(jsonUrl, AttackPattern.class)
-
-        then:
-        assert attackPattern.getType() == 'attack-pattern'
-        assert attackPattern.getId() == 'attack-pattern--477b763e-226f-46b3-a211-3cb5b86978a6'
-        assert attackPattern.getCreated() == Instant.parse('2018-11-26T01:17:26.416Z').atZone(ZoneId.of("Etc/UTC"))
-        assert attackPattern.getModified() == Instant.parse('2018-11-29T01:17:26.416Z').atZone(ZoneId.of("Etc/UTC"))
-        assert attackPattern.revoked == false
-        assert attackPattern.getName() == "some pattern"
-
-
-        assert attackPattern.getKillChainPhases().size() == 2
-
-        LinkedHashSet<KillChainPhase> kcpSet = new LinkedHashSet<>()
-        kcpSet.add(new KillChainPhase('Chain1', 'phase1'))
-        kcpSet.add(new KillChainPhase('Chain1', 'phase2'))
-        assert attackPattern.getKillChainPhases() == kcpSet
-
-        assert attackPattern.getObjectMarkingRefs().size() == 1
-        assert attackPattern.getObjectMarkingRefsIds()[0] == 'marking-definition--b17db0c7-1c2e-4c6a-9cab-34d0cacfbf50'
-        assert attackPattern.getObjectMarkingRefs()[0].hasObject() == false
-        assert attackPattern.getObjectMarkingRefs()[0].getId() == 'marking-definition--b17db0c7-1c2e-4c6a-9cab-34d0cacfbf50'
-
-    }
-
-    def "AttackPattern 4 json"(){
-
-        when: "load AttackPattern4.json"
-        URL jsonUrl = getClass().getResource('/stix/json/domainobjects/AttackPattern4.json')
+        when: "load AttackPattern-Full-1.json"
+        URL jsonUrl = getClass().getResource('/stix/json/domainobjects/AttackPattern-Full-1.json')
 
         ObjectMapper objectMapper = StixDataFormats.getJsonMapper()
 
