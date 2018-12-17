6ad5a290d14e8ec0f2e166022624ee559a04ae73
StephenOTT <stephenrussett@gmail.com>
2018-12-17 12:37:11 -0500

    Fix validation error for marking defitnions for dehydrated Data markings

--- src/main/java/io/digitalstate/stix/validation/contraints/markingdefinitiontype/StixMarkingDefinitionTypeLimitValidator.java
+++ src/main/java/io/digitalstate/stix/validation/contraints/markingdefinitiontype/StixMarkingDefinitionTypeLimitValidator.java
@@ -22,47 +22,52 @@ public class StixMarkingDefinitionTypeLimitValidator implements ConstraintValida
     public boolean isValid(MarkingDefinitionDm markingDefinitionDm,
                            ConstraintValidatorContext cxt) {
 
-        // Throws error is the definition property is null.
-        // This is semi duplication of the notNull annotation on the attribute
-        if (markingDefinitionDm.getDefinition() == null){
-            String violationMessage = "Definition attribute of a Marking Definition cannot be null";
-            cxt.buildConstraintViolationWithTemplate(violationMessage).addConstraintViolation();
-            return false;
-
-            // Default Value helper to populate the definition_type attribute based on the definition  attribute's class
-        } else if (markingDefinitionDm.getDefinitionType() == null) {
-            if (markingObject.isAssignableFrom(markingDefinitionDm.getDefinition().getClass())) {
-                try {
-                    Field typeField = markingDefinitionDm.getClass().getDeclaredField("definitionType");
-                    typeField.setAccessible(true);
-                    typeField.set(markingDefinitionDm, markingDefinitionType);
-
-                } catch (NoSuchFieldException e) {
-                    String violationMessage = "Cant find Field: 'definitionType' for: " + markingObject;
-                    cxt.buildConstraintViolationWithTemplate(violationMessage).addConstraintViolation();
-                    e.printStackTrace();
-                    return false;
-
-                } catch (IllegalAccessException e) {
-                    String violationMessage = "Illegal Access Exception for: 'definitionType' for: " + markingObject;
-                    cxt.buildConstraintViolationWithTemplate(violationMessage).addConstraintViolation();
-                    e.printStackTrace();
-                    return false;
-                }
-            }
-        }
-
-        // Checks if the definition and the definition type match as per the spec and class requirements
-        // Requirements are stated in the class level(type) of the Marking Definition.
-        if (markingObject.isAssignableFrom(markingDefinitionDm.getDefinition().getClass())){
-            if (markingDefinitionDm.getDefinitionType().equals(markingDefinitionType)){
-                return true;
-            } else {
-                String violationMessage = "Marking Definition Type is incorrect for Marking Object";
+        if (markingDefinitionDm.getHydrated()) {
+            // Throws error is the definition property is null.
+            // This is semi duplication of the notNull annotation on the attribute
+            if (markingDefinitionDm.getDefinition() == null) {
+                String violationMessage = "Definition attribute of a Marking Definition cannot be null";
                 cxt.buildConstraintViolationWithTemplate(violationMessage).addConstraintViolation();
                 return false;
+
+                // Default Value helper to populate the definition_type attribute based on the definition  attribute's class
+            } else if (markingDefinitionDm.getDefinitionType() == null) {
+                if (markingObject.isAssignableFrom(markingDefinitionDm.getDefinition().getClass())) {
+                    try {
+                        Field typeField = markingDefinitionDm.getClass().getDeclaredField("definitionType");
+                        typeField.setAccessible(true);
+                        typeField.set(markingDefinitionDm, markingDefinitionType);
+
+                    } catch (NoSuchFieldException e) {
+                        String violationMessage = "Cant find Field: 'definitionType' for: " + markingObject;
+                        cxt.buildConstraintViolationWithTemplate(violationMessage).addConstraintViolation();
+                        e.printStackTrace();
+                        return false;
+
+                    } catch (IllegalAccessException e) {
+                        String violationMessage = "Illegal Access Exception for: 'definitionType' for: " + markingObject;
+                        cxt.buildConstraintViolationWithTemplate(violationMessage).addConstraintViolation();
+                        e.printStackTrace();
+                        return false;
+                    }
+                }
+            }
+
+            // Checks if the definition and the definition type match as per the spec and class requirements
+            // Requirements are stated in the class level(type) of the Marking Definition.
+            if (markingObject.isAssignableFrom(markingDefinitionDm.getDefinition().getClass())) {
+                if (markingDefinitionDm.getDefinitionType().equals(markingDefinitionType)) {
+                    return true;
+                } else {
+                    String violationMessage = "Marking Definition Type is incorrect for Marking Object";
+                    cxt.buildConstraintViolationWithTemplate(violationMessage).addConstraintViolation();
+                    return false;
+                }
+            } else {
+                return true;
             }
         } else {
+            // If the object is not hydrated, then this logic does not matter
             return true;
         }
     }
