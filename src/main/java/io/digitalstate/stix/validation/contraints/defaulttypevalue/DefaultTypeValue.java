a4c03d30c0752ed954aaa37def521ce2c1f316da
StephenOTT <stephen@digitalstate.ca>
2019-01-18 12:03:06 -0500

    Update DefaultTypeValue generation for Cyber Observables

--- src/main/java/io/digitalstate/stix/validation/contraints/defaulttypevalue/DefaultTypeValue.java
+++ src/main/java/io/digitalstate/stix/validation/contraints/defaulttypevalue/DefaultTypeValue.java
@@ -14,13 +14,13 @@ import static java.lang.annotation.ElementType.TYPE;
 /**
  * <p>Sets the default value of the {@code type} field for a Bundleable object.
  * This annotation must only be applied on a Class level of a class that extends from Bundleable object
- * (Typically SDOs, SROs, and Marking Definitions).</p>
+ * (Typically SDOs, SROs, COOs, and Marking Definitions).</p>
  * <br>
  * <p>This annotation will also validate a manually set {@code type} attribute.
  * If the {@code type} attribute does not equal the value set in this annotation, then validation will fail.</p>
  */
 @Documented
-@Constraint(validatedBy = {StixDefaultTypeValueValidator.class, StixDefaultTypeValueBundleObjectValidator.class})
+@Constraint(validatedBy = {StixDefaultTypeValueValidator.class, StixDefaultTypeValueBundleObjectValidator.class, StixDefaultTypeValueCyberObservableValidator.class})
 @Target( { ANNOTATION_TYPE, TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DefaultTypeValue {
