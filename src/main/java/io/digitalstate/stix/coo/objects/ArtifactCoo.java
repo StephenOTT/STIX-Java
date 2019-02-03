1b73f237a14722a44ed633a96d74a616c61392b4
StephenOTT <stephen@digitalstate.ca>
2019-02-03 12:10:03 -0500

    Remove redactable for now

--- src/main/java/io/digitalstate/stix/coo/objects/ArtifactCoo.java
+++ src/main/java/io/digitalstate/stix/coo/objects/ArtifactCoo.java
@@ -40,13 +40,11 @@ public interface ArtifactCoo extends CyberObservableObject {
 	@JsonProperty("mime_type") @JsonInclude(value = NON_EMPTY, content= NON_EMPTY)
 	@JsonPropertyDescription("The value of this property MUST be a valid MIME type as specified in the IANA Media Types registry.")
 	@OptionalPattern(regexp = "^(application|audio|font|image|message|model|multipart|text|video)/[a-zA-Z0-9.+_-]+")
-    @Redactable
     Optional<String> getMimeType();
 
     @JsonProperty("payload_bin") @JsonInclude(value = NON_EMPTY, content= NON_EMPTY)
 	@JsonPropertyDescription("Specifies the binary data contained in the artifact as a base64-encoded string.")
 	@OptionalPattern(regexp = "^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$")
-    @Redactable
     Optional<String> getPayloadBin();
 
 	/**
@@ -58,7 +56,7 @@ public interface ArtifactCoo extends CyberObservableObject {
     @JsonProperty("url") @JsonInclude(value = NON_EMPTY, content= NON_EMPTY)
     @JsonPropertyDescription("The value of this property MUST be a valid URL that resolves to the unencoded content.")
     @OptionalPattern(regexp = "^(?:(?:https?|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?!(?:10|127)(?:\\.\\d{1,3}){3})(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\\.(?:[a-z\u00a1-\uffff]{2,}))\\.?)(?::\\d{2,5})?(?:[/?#]\\S*)?$")
-    @Redactable
+    //@TODO review if the @Url constraint can be used instead.
     Optional<String> getUrl();
 
     //@TODO review logic requirements for Redactable on Hash values
