aa95b03cc0530e948873893319a5455aa554bd47
StephenOTT <stephen@digitalstate.ca>
2019-02-03 13:48:27 -0500

    Remove old value and add jsonprop name for brevity

--- src/main/java/io/digitalstate/stix/coo/extension/CyberObservableExtensionCommonProperties.java
+++ src/main/java/io/digitalstate/stix/coo/extension/CyberObservableExtensionCommonProperties.java
@@ -1,13 +1,13 @@
 package io.digitalstate.stix.coo.extension;
 
-import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.immutables.value.Value;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Value.Style(validationMethod = Value.Style.ValidationMethod.NONE)
-//@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", include = JsonTypeInfo.As.EXISTING_PROPERTY)
 public interface CyberObservableExtensionCommonProperties {
 
     /**
@@ -16,6 +16,7 @@ public interface CyberObservableExtensionCommonProperties {
      */
     @NotBlank
     @JsonIgnore
+    @JsonProperty("type")
     @Size(min = 3, max = 250)
     String getType();
 
