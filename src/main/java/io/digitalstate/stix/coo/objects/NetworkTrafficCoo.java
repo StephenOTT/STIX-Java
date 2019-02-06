f1125cdd17bca3f5bb2fb93fafb53da56748aa51
StephenOTT <stephen@digitalstate.ca>
2019-02-06 17:04:12 -0500

    fix missing configs

--- src/main/java/io/digitalstate/stix/coo/objects/NetworkTrafficCoo.java
+++ src/main/java/io/digitalstate/stix/coo/objects/NetworkTrafficCoo.java
@@ -7,13 +7,12 @@ import io.digitalstate.stix.coo.CyberObservableObject;
 import io.digitalstate.stix.helpers.StixDataFormats;
 import io.digitalstate.stix.validation.contraints.defaulttypevalue.DefaultTypeValue;
 import io.digitalstate.stix.validation.groups.DefaultValuesProcessor;
+import org.hibernate.validator.constraints.Range;
 import org.immutables.serial.Serial;
 import org.immutables.value.Value;
 
 import javax.validation.Valid;
-import javax.validation.constraints.DecimalMax;
-import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
@@ -60,26 +59,24 @@ public interface NetworkTrafficCoo extends CyberObservableObject {
      */
     @JsonProperty("src_ref")
     @JsonPropertyDescription("Specifies the source of the network traffic, as a reference to one or more Observable Objects.")
-    Optional<String> srcRef();
+    Optional<String> getSrcRef();
 
     /*
      * Must be of type ipv4-addr, ipv6-addr, mac-addr, or domain-name
      */
     @JsonProperty("dst_ref")
     @JsonPropertyDescription("Specifies the destination of the network traffic, as a reference to one or more Observable Objects.")
-    Optional<String> dstRef();
+    Optional<String> getDstRef();
 
     @JsonProperty("src_port")
     @JsonPropertyDescription("Specifies the source port used in the network traffic, as an integer. The port value MUST be in the range of 0 - 65535.")
-    @DecimalMin("0")
-    @DecimalMax("65535")
-    Optional<Integer> srcPort();
+    @Range(min = 0, max = 65535 )
+    Optional<Integer> getSrcPort();
 
     @JsonProperty("dst_port")
     @JsonPropertyDescription("Specifies the destination port used in the network traffic, as an integer. The port value MUST be in the range of 0 - 65535.")
-    @DecimalMin("0")
-    @DecimalMax("65535")
-    Optional<Integer> dstPort();
+    @Range(min = 0, max = 65535)
+    Optional<Integer> getDstPort();
 
     /*
      * Specifies the protocols observed in the network traffic, along with their
@@ -92,59 +89,57 @@ public interface NetworkTrafficCoo extends CyberObservableObject {
     @JsonProperty("protocols")
     @JsonPropertyDescription("Specifies the protocols observed in the network traffic, along with their corresponding state.")
     @NotNull
-    Set<String> protocols();
+    Set<String> getProtocols();
 
     @JsonProperty("src_byte_count")
     @JsonPropertyDescription("Specifies the number of bytes sent from the source to the destination.")
-    Optional<Integer> srcByteCount();
+    Optional<Integer> getSrcByteCount();
 
     @JsonProperty("dst_byte_count")
     @JsonPropertyDescription("Specifies the number of bytes sent from the destination to the source.")
-    Optional<Integer> dstByteCount();
+    Optional<Integer> getDstByteCount();
 
     @JsonProperty("src_packets")
     @JsonPropertyDescription("Specifies the number of packets sent from the source to the destination.")
-    Optional<Integer> srcPackets();
+    Optional<Integer> getSrcPackets();
 
     @JsonProperty("dst_packets")
     @JsonPropertyDescription("Specifies the number of packets sent destination to the source.")
-    Optional<Integer> dstPackets();
+    Optional<Integer> getDstPackets();
 
     /*
      * Objects much be Integers or Strings
      */
     @JsonProperty("ipfix")
     @JsonPropertyDescription("Specifies any IP Flow Information Export [IPFIX] data for the traffic, as a dictionary.")
-    @Valid
-    Map<String,Object> ipfix();
+    Map<String,Object> getIpFix();
 
     /*
      * Must be of type artifact
      */
     @JsonProperty("src_payload_ref")
     @JsonPropertyDescription("Specifies the bytes sent from the source to the destination.")
-    Optional<String> srcPayloadRef();
+    Optional<String> getSrcPayloadRef();
 
     /*
      * Must be of type artifact
      */
     @JsonProperty("dst_payload_ref")
     @JsonPropertyDescription("Specifies the bytes sent from the source to the destination.")
-    Optional<String> dstPayloadRef();
+    Optional<String> getDstPayloadRef();
 
     /*
      * Must be of type network-traffic
      */
     @JsonProperty("encapsulates_refs")
     @JsonPropertyDescription("Links to other network-traffic objects encapsulated by a network-traffic.")
-    @Valid
-    List<String> encapsulatesRefs();
+    List<String> getEncapsulatesRefs();
 
     /*
      * Must be of type network-traffic
      */
     @JsonProperty("encapsulated_by_ref")
     @JsonPropertyDescription("Links to another network-traffic object which encapsulates this object.")
-    Optional<String> encapsulatedByRef();
+    Optional<String> getEncapsulatedByRef();
 
 }
