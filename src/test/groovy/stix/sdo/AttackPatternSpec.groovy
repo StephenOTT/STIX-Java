03fc0074e2e00f60b0e018fb7b511346ff219138
StephenOTT <stephen@digitalstate.ca>
2019-02-14 16:05:03 -0500

    Update attack pattern with use of objectmapper and json assert

--- src/test/groovy/stix/sdo/AttackPatternSpec.groovy
+++ src/test/groovy/stix/sdo/AttackPatternSpec.groovy
@@ -1,11 +1,18 @@
 package stix.sdo
 
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.digitalstate.stix.json.StixParsers
 import io.digitalstate.stix.sdo.objects.AttackPattern
+import org.skyscreamer.jsonassert.JSONAssert
+import org.skyscreamer.jsonassert.JSONCompareMode
+import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Unroll
 
 class AttackPatternSpec extends Specification implements StixMockDataGenerator {
+
+    @Shared ObjectMapper mapper = new ObjectMapper()
 
     @Unroll
     def "Generate Attack Pattern Data: Run: '#i'"() {
@@ -14,11 +21,12 @@ class AttackPatternSpec extends Specification implements StixMockDataGenerator {
             println "Original Object: ${originalAttackPattern.toString()}"
 
         then: "Convert Attack Pattern to Json"
-            String originalJson = originalAttackPattern.toJsonString()
-            println "Original Json: ${originalJson}"
+            JsonNode originalJson = mapper.readTree(originalAttackPattern.toJsonString())
+            String originalJsonString = mapper.writeValueAsString(originalJson)
+            println "Original Json: ${originalJsonString}"
 
         then: "Parse Json back into Attack Pattern Object"
-            AttackPattern parsedAttackPattern = (AttackPattern)StixParsers.parseObject(originalJson)
+            AttackPattern parsedAttackPattern = (AttackPattern)StixParsers.parseObject(originalJsonString)
             println "Parsed Object: ${parsedAttackPattern}"
 
         //@TODO needs to be setup to handle dehydrated object comparison
@@ -26,11 +34,12 @@ class AttackPatternSpec extends Specification implements StixMockDataGenerator {
 //            assert originalAttackPattern == parsedAttackPattern
 
         then: "Convert Parsed Attack Pattern Object back to into Json"
-            String newJson = parsedAttackPattern.toJsonString()
-            println "New Json: ${newJson}"
+            JsonNode newJson =  mapper.readTree(parsedAttackPattern.toJsonString())
+            String newJsonString = mapper.writeValueAsString(newJson)
+            println "New Json: ${newJsonString}"
 
         then: "New Json should match Original Json"
-            assert newJson == originalJson
+            JSONAssert.assertEquals(originalJsonString, newJsonString, JSONCompareMode.NON_EXTENSIBLE)
 
         where:
             i << (1..100)
