7377b11297f7416bf44f14b6e0b7a88d2a0a4d74
StephenOTT <stephen@digitalstate.ca>
2019-05-13 06:11:25 -0400

    cleanup spec

--- src/test/groovy/stix/datamarkings/TLPmarkingsSpec.groovy
+++ src/test/groovy/stix/datamarkings/TLPmarkingsSpec.groovy
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package stix.datamarkings
 
 import org.skyscreamer.jsonassert.JSONAssert
@@ -17,80 +14,77 @@ import io.digitalstate.stix.sdo.objects.Indicator
 import spock.lang.Shared
 import spock.lang.Specification
 
-/**
- * @author Kathy Lee Simunich, Argonne National Laboratory
- *
- */
-class TLPmarkingsSpec extends Specification {
-	
-	@Shared ObjectMapper mapper = new ObjectMapper()
-	
-	
-	def "test TLP"() {
-		when: "create tlp white"
-		
-		MarkingDefinition originalMarkingDefinition = Tlps.TLP_WHITE;
-		
+class TlpMarkingsSpec extends Specification {
+
+    @Shared
+    ObjectMapper mapper = new ObjectMapper()
+
+    def "TLP Defaults Creation Test: WHITE"() {
+        when: "Create TLP:white from pre-built TLPs"
+
+        MarkingDefinition originalMarkingDefinition = Tlps.TLP_WHITE
+
         then: "Convert Marking Definition to Json"
             JsonNode originalJson = mapper.readTree(originalMarkingDefinition.toJsonString())
             String originalJsonString = mapper.writeValueAsString(originalJson)
-            println "Original Json: ${originalJsonString}"
+//            println "Original Json: ${originalJsonString}"
 
         then: "Parse Json back into Marking Definition Object"
-            MarkingDefinition parsedMarkingDefinition = (MarkingDefinition)StixParsers.parseObject(originalJsonString)
-            println "Parsed Object: ${parsedMarkingDefinition}"
+            MarkingDefinition parsedMarkingDefinition = (MarkingDefinition) StixParsers.parseObject(originalJsonString)
+             MarkingDefinition parsedMarkingDefinitionGeneric = StixParsers.parse(originalJsonString, MarkingDefinition.class)
+//            println "Parsed Object: ${parsedMarkingDefinition}"
 
-        //@TODO needs to be setup to handle dehydrated object comparison
-//        then: "Parsed object should match Original object"
-//            assert originalMarkingDefinition == parsedMarkingDefinition
+            //@TODO needs to be setup to handle dehydrated object comparison
+    //        then: "Parsed object should match Original object"
+    //            assert originalMarkingDefinition == parsedMarkingDefinition
 
         then: "Convert Parsed Marking Definition Object back to into Json"
-            JsonNode newJson =  mapper.readTree(parsedMarkingDefinition.toJsonString())
+            JsonNode newJson = mapper.readTree(parsedMarkingDefinition.toJsonString())
             String newJsonString = mapper.writeValueAsString(newJson)
-            println "New Json: ${newJsonString}"
+//            println "New Json: ${newJsonString}"
 
         then: "New Json should match Original Json"
             JSONAssert.assertEquals(originalJsonString, newJsonString, JSONCompareMode.NON_EXTENSIBLE)
-	
-	}
-	
-	def "test indicator with TLP marking"() {
-		when: "create tlp green"
-		
-		MarkingDefinition green = Tlps.TLP_GREEN;
-		StixInstant now = new StixInstant();
-		
-		Indicator ind = Indicator.builder()
-						.id("indicator--59ccb738-921a-4941-8ab2-33da522bd4e1")
-						.created(now)
-						.modified(now)
-						.labels(Arrays.asList("malicious-activity"))
-						.name("128.0.0.1")
-						.pattern("[ipv4-addr:value = '128.0.0.1']")
-						.validFrom(now)
-						.objectMarkingRefs(Arrays.asList(green))
-						.build();
-		
-		then: "Convert Marking Definition to Json"
-			JsonNode originalJson = mapper.readTree(ind.toJsonString())
-			String originalJsonString = mapper.writeValueAsString(originalJson)
-			println "Original Json: ${originalJsonString}"
+    }
 
-		then: "Parse Json back into Marking Definition Object"
-			Indicator parsed = (Indicator)StixParsers.parseObject(originalJsonString)
-			println "Parsed Object: ${parsed}"
+    def "Test indicator with Default TLP markings"() {
+        when: "Create TLP:green"
 
-		//@TODO needs to be setup to handle dehydrated object comparison
-//        then: "Parsed object should match Original object"
-//            assert originalMarkingDefinition == parsedMarkingDefinition
+            MarkingDefinition TlpGreen = Tlps.TLP_GREEN
+            StixInstant now = new StixInstant()
 
-		then: "Convert Parsed Marking Definition Object back to into Json"
-			JsonNode newJson =  mapper.readTree(parsed.toJsonString())
-			String newJsonString = mapper.writeValueAsString(newJson)
-			println "New Json: ${newJsonString}"
+            Indicator ind = Indicator.builder()
+                    .id("indicator--59ccb738-921a-4941-8ab2-33da522bd4e1")
+                    .created(now)
+                    .modified(now)
+                    .addLabel("malicious-activity")
+                    .name("128.0.0.1")
+                    .pattern("[ipv4-addr:value = '128.0.0.1']")
+                    .validFrom(now)
+                    .addObjectMarkingRef(TlpGreen)
+                    .build()
 
-		then: "New Json should match Original Json"
-			JSONAssert.assertEquals(originalJsonString, newJsonString, JSONCompareMode.NON_EXTENSIBLE)
-	
-	}
+        then: "Convert Marking Definition to Json"
+            JsonNode originalJson = mapper.readTree(ind.toJsonString())
+            String originalJsonString = mapper.writeValueAsString(originalJson)
+//            println "Original Json: ${originalJsonString}"
+
+        then: "Parse Json back into Marking Definition Object"
+            Indicator parsed = (Indicator) StixParsers.parseObject(originalJsonString)
+            Indicator parsedGeneric = StixParsers.parse(originalJsonString, Indicator.class)
+//            println "Parsed Object: ${parsed}"
+
+            //@TODO needs to be setup to handle dehydrated object comparison
+    //        then: "Parsed object should match Original object"
+    //            assert originalMarkingDefinition == parsedMarkingDefinition
+
+        then: "Convert Parsed Marking Definition Object back to into Json"
+            JsonNode newJson = mapper.readTree(parsed.toJsonString())
+            String newJsonString = mapper.writeValueAsString(newJson)
+//            println "New Json: ${newJsonString}"
+
+        then: "New Json should match Original Json"
+            JSONAssert.assertEquals(originalJsonString, newJsonString, JSONCompareMode.NON_EXTENSIBLE)
+
+    }
 }
