17b8d42b280ad5cff70f87bc7e792e1d435aaec9
StephenOTT <stephen@digitalstate.ca>
2019-11-15 21:12:56 -0500

    bundle cleanup

--- src/main/kotlin/com/stephenott/stix/StixBundle.kt
+++ src/main/kotlin/com/stephenott/stix/StixBundle.kt
@@ -1,23 +1,36 @@
 package com.stephenott.stix
 
+import com.stephenott.stix.common.BusinessRulesValidator
+import com.stephenott.stix.common.CompanionStixType
 import com.stephenott.stix.objects.StixObject
+import com.stephenott.stix.objects.core.sco.objects.ArtifactSco
 import com.stephenott.stix.type.StixIdentifier
 import com.stephenott.stix.type.StixType
 
-interface StixBundle: StixContent{
+interface StixBundle : StixContent {
     val objects: LinkedHashSet<StixObject>
 
-    companion object {
-        val stixType = StixType("bundle")
+    companion object :
+            CompanionStixType,
+            BusinessRulesValidator<StixBundle> {
+
+        override val stixType = StixType("bundle")
+
+        override fun objectValidationRules(obj: StixBundle, stixRegistries: StixRegistries) {
+            //@TODO
+        }
     }
 }
-
 
 
 data class Bundle(override val type: StixType = StixBundle.stixType,
                   override val id: StixIdentifier = StixIdentifier(type),
                   override val objects: LinkedHashSet<StixObject>,
                   override val stixRegistries: StixRegistries = Stix.defaultRegistries
-): StixBundle{
+) : StixBundle {
+
+    init {
+        StixBundle.objectValidationRules(this, stixRegistries)
+    }
 
 }
